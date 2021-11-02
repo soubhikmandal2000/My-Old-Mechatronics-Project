bb89873c33355f95f4030be51ff5db626e9fddf7
Soubhik Mandal <74180657+soubhikmandal2000@users.noreply.github.com>
2021-11-02 14:13:59 +0530

    Add files via upload

---
+++ RC Transmitter/Receiver side - Robot Car.ino
@@ -0,0 +1,160 @@
+/*
+   Arduino Robot Car Wireless Control - RC control using the NRF24L01 Transceiver module
+   = Receiver code =
+   by Dejan Nedelkovski, www.HowToMechatronics.com
+   Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#define enA 5   // Note: Pin 9 in previous video ( pin 10 is used for the SPI communication of the NRF24L01)
+#define in1 3
+#define in2 4
+#define enB 6   // Note:  Pin 10 in previous video
+#define in3 7
+#define in4 8
+RF24 radio(9, 10);   // nRF24L01 (CE, CSN)
+const byte address[6] = "00001";
+unsigned long lastReceiveTime = 0;
+unsigned long currentTime = 0;
+
+// Max size of this struct is 32 bytes
+struct Data_Package {
+  byte j1PotX;
+  byte j1PotY;
+  byte j1Button;
+  byte j2PotX;
+  byte j2PotY;
+  byte j2Button;
+  byte pot1;
+  byte pot2;
+  byte tSwitch1;
+  byte tSwitch2;
+  byte button1;
+  byte button2;
+  byte button3;
+  byte button4;
+};
+
+Data_Package data; //Create a variable with the above structure
+
+int  xAxis, yAxis;
+int motorSpeedA = 0;
+int motorSpeedB = 0;
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening(); //  Set the module as receiver
+  resetData();
+}
+void loop() {
+  // Check whether we keep receving data, or we have a connection between the two modules
+  currentTime = millis();
+  if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
+    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone jas a throttle up, if we lose connection it can keep flying away if we dont reset the function
+  }
+  // Check whether there is data to be received
+  if (radio.available()) {
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+    lastReceiveTime = millis(); // At this moment we have received the data
+  }
+  xAxis = data.j1PotX;
+  yAxis = data.j1PotY;
+
+  // Y-axis used for forward and backward control
+  if (yAxis < 110) {
+    // Set Motor A backward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // Set Motor B backward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // Convert the declining Y-axis readings for going backward from 110 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(yAxis, 110, 0, 0, 255);
+    motorSpeedB = map(yAxis, 110, 0, 0, 255);
+    Serial.print(motorSpeedA);
+    Serial.print("  ");
+    Serial.println(yAxis);
+  }
+  else if (yAxis > 140) {
+    // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // Set Motor B forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // Convert the increasing Y-axis readings for going forward from 140 to 255 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(yAxis, 140, 255, 0, 255);
+    motorSpeedB = map(yAxis, 140, 255, 0, 255);
+  }
+  // If joystick stays in middle the motors are not moving
+  else {
+    motorSpeedA = 0;
+    motorSpeedB = 0;
+  }
+  // X-axis used for left and right control
+  if (xAxis < 110) {
+    // Convert the declining X-axis readings from 140 to 255 into increasing 0 to 255 value
+    int xMapped = map(xAxis, 110, 0, 0, 255);
+    // Move to left - decrease left motor speed, increase right motor speed
+    motorSpeedA = motorSpeedA - xMapped;
+    motorSpeedB = motorSpeedB + xMapped;
+    // Confine the range from 0 to 255
+    if (motorSpeedA < 0) {
+      motorSpeedA = 0;
+    }
+    if (motorSpeedB > 255) {
+      motorSpeedB = 255;
+    }
+  }
+  if (xAxis > 140) {
+    // Convert the increasing X-axis readings from 110 to 0 into 0 to 255 value
+    int xMapped = map(xAxis, 140, 255, 0, 255);
+    // Move right - decrease right motor speed, increase left motor speed
+    motorSpeedA = motorSpeedA + xMapped;
+    motorSpeedB = motorSpeedB - xMapped;
+    // Confine the range from 0 to 255
+    if (motorSpeedA > 255) {
+      motorSpeedA = 255;
+    }
+    if (motorSpeedB < 0) {
+      motorSpeedB = 0;
+    }
+  }
+  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
+  if (motorSpeedA < 70) {
+    motorSpeedA = 0;
+  }
+  if (motorSpeedB < 70) {
+    motorSpeedB = 0;
+  }
+  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+  analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+}
+void resetData() {
+  // Reset the values when there is no radio connection - Set initial default values
+  data.j1PotX = 127;
+  data.j1PotY = 127;
+  data.j2PotX = 127;
+  data.j2PotY = 127;
+  data.j1Button = 1;
+  data.j2Button = 1;
+  data.pot1 = 1;
+  data.pot2 = 1;
+  data.tSwitch1 = 1;
+  data.tSwitch2 = 1;
+  data.button1 = 1;
+  data.button2 = 1;
+  data.button3 = 1;
+  data.button4 = 1;
+}
